d739150d12fed7650e786b0d39691fd5e95040ec
Global Young <67513038+YoungHaKim7@users.noreply.github.com>
2026-03-05 18:26:20 +0900

    Update configuration.nix

--- configuration.nix
+++ configuration.nix
@@ -150,6 +150,16 @@ in
     curl
   ];
 
+  ######
+  # dynamic libraries 
+  ######
+  #programs.nix-ld.enable = true;
+  #programs.nix-ld.libraries = with pkgs; [
+    # Add any missing dynamic libraries for unpackaged programs
+    # here, NOT in environment.systemPackages
+  #];
+
+
   # -----------------------------
   # Graphics
   # -----------------------------
