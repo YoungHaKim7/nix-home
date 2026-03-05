77954cfed8c1c0a066df3a06ac3d333136c8810f
Global Young <67513038+YoungHaKim7@users.noreply.github.com>
2026-03-05 18:26:55 +0900

    https://nix.dev/permalink/stub-ld

--- configuration.nix
+++ configuration.nix
@@ -153,6 +153,8 @@ in
   ######
   # dynamic libraries 
   ######
+  # https://nix.dev/permalink/stub-ld
+  #
   #programs.nix-ld.enable = true;
   #programs.nix-ld.libraries = with pkgs; [
     # Add any missing dynamic libraries for unpackaged programs
